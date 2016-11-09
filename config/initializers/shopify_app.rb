1c25bde5a07bedde862481b41b913fdf3ef30861
kevinchau321 <kevinchau321@gmail.com>
2016-11-08 18:29:35 -0800

    removed redirect_uri

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -2,7 +2,6 @@ ShopifyApp.configure do |config|
 
   config.api_key = "<api_key>"
   config.secret = "<secret>"
-  config.redirect_uri = "http://pseudoskatecoapp.herokuapp.com/auth/shopify/callback"
   config.scope = "read_orders, read_products"
   config.embedded_app = true
 end
