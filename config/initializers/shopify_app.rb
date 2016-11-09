f1fc2c0f73cecac0da941fa650b615de18314d62
kevinchau321 <kevinchau321@gmail.com>
2016-11-08 18:49:28 -0800

    added redirect uri

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -2,6 +2,7 @@ ShopifyApp.configure do |config|
 
   config.api_key = "8a51c97b3ed5ff0b42b3964ca2a9f9da"
   config.secret = "2e28cb21b31d4534d24364d170261994"
+  config.redirect_uri = "http://pseudoskatecoapp.herokuapp.com/auth/shopify/callback"
   config.scope = "read_orders, read_products"
   config.embedded_app = true
 end
