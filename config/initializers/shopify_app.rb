fab551d049c02b922965939ec092b70a82937b0a
kevinchau321 <kevinchau321@gmail.com>
2016-11-08 18:45:20 -0800

    trying new secret key

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,7 +1,7 @@
 ShopifyApp.configure do |config|
 
-  config.api_key = "<api_key>"
-  config.secret = "<secret>"
+  config.api_key = "8a51c97b3ed5ff0b42b3964ca2a9f9da"
+  config.secret = "2e28cb21b31d4534d24364d170261994"
   config.scope = "read_orders, read_products"
   config.embedded_app = true
 end
